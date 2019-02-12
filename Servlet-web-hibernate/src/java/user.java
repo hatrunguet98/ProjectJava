0df87263340f457bdd4d9a80e7ea1a1bdc88077c
HaTrung <hatrunguet98@gmail.com>
2019-02-12 09:57:11 +0700

    1

---
+++ Servlet-web-hibernate/src/java/user.java
@@ -0,0 +1,51 @@
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ha Trung
+ */
+@Entity
+@Table(name="user")
+public class user implements Serializable {
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private int id;
+    @Column(name="name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" + "id=" + id + ", name=" + name + '}';
+    }
+    
+    
+}
